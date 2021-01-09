363c4cfbaab97fb979214e90e1d456e9aaded5f3
Serhij Zhenzhera <73667238+SerhijZhenzhera@users.noreply.github.com>
2021-01-09 05:19:27 +0200

    number length

---
+++ checkio.org/elementary/number_length.py
@@ -0,0 +1,10 @@
+'''
+You have a positive integer. Try to find out how many digits it has?
+Input: A positive Int
+Output: An Int.
+'''
+
+
+def number_length(a: int) -> int:
+    return len(str(a))
+    return None

b9236698c6f9a50c7c61ae1dbd0bf49863997fc3
Serhij Zhenzhera <73667238+SerhijZhenzhera@users.noreply.github.com>
2021-01-15 00:16:53 +0200

    replace first

---
+++ checkio.org/elementary/replace_first.py
@@ -0,0 +1,25 @@
+'''
+In a given list the first element should become the last one.
+An empty list or list with only one element should stay the same. 
+Input: List.
+Output: Iterable. 
+'''
+
+# агрессивное решение без условной конструкции
+def replace_first(items: list) -> Iterable:
+    items.append('replace_first_mission') # для items = []
+    rem_item = items.pop(0)
+    items.append(rem_item)
+    items.remove('replace_first_mission')
+    return items
+
+
+# безопасное решение
+def replace_first(items):
+    if not items == []:
+        rem_item = items.pop(0)
+        items.append(rem_item)
+    else:
+        pass
+    return items
+    

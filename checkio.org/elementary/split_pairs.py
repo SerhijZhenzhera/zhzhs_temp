73b1bc7d67802ae31a5d36ab4fef3fbcd1f4222f
Serhij Zhenzhera <73667238+SerhijZhenzhera@users.noreply.github.com>
2021-01-17 02:07:53 +0200

    split pairs reload

--- checkio.org/elementary/split_pairs.py
+++ checkio.org/elementary/split_pairs.py
@@ -7,31 +7,9 @@ Precondition: 0<=len(str)<=100
 '''
 
 
-
-
-
 def split_pairs(a):
     b = []
     if not len(a)%2 == 0: a += '_'
     for i in range(0, len(a)-1, 2):
         b.append(''.join([a[i], a[i+1]]))
-    print(b)    
-        
-
-    
-    
-    
-    # print([a[: len(a)//2], a[len(a)//2:]])
-    # return [a[: len(a)//2], a[len(a)//2:]]
-    
-    
-    
-
-    
-
-    
-    
-    
-
-split_pairs('abcd')
-split_pairs('abc')
+    return b  
